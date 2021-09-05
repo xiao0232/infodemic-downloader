975fff33f4f61f6f3e414186b1c224ca00f73b82
xiao0232 <t409828543087@gmail.com>
2021-09-05 11:54:05 +0900

    add: debug用環境整備

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,32 @@ import { ConfigEnv, defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import reactJsx from 'vite-react-jsx'
 
+import { visualizer } from 'rollup-plugin-visualizer'
+// import viteCompression from 'vite-plugin-compression'
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }: ConfigEnv) => ({
   root: './',
   plugins: [reactRefresh(), reactJsx()],
   define: mode === 'development' && {
     global: {},
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        mode === 'visualize' && visualizer({ filename: './dist/stats.html' }),
+      ],
+      //     output: {
+      //       manualChunks(id) {
+      //         if (id.includes('node_modules')) {
+      //           return id
+      //             .toString()
+      //             .split('node_modules/')[1]
+      //             .split('/')[0]
+      //             .toString()
+      //         }
+      //       },
+      //     },
+    },
   },
 }))
